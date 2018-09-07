d52585b3381d9d18b0a5282ad827a949319dd70f
guishangquan <guishangquan@szzbmy.com>
2018-09-07 12:00:26 +0800

    zuul

---
+++ springcloud-zuul/src/main/java/com/gsq/learning/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.gsq.learning.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author guishangquan
+ * @date 2018/9/7
+ */
+//@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
